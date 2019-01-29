1043694c239ea0f666e7ee7518607ea03e4c282c
Hidde L <mail.hidde@gmail.com>
2019-01-29 21:27:05 +0100

    Fix light_patterns

--- week12/light_pattern/main.cpp
+++ week12/light_pattern/main.cpp
@@ -24,104 +24,28 @@ void testcase() {
     int totalCount = 0;
 //    int numMovesRequired = 0;
     int oneCount = 0;
-    std::vector<bool> bulbs(N);
+    std::vector<std::vector<int>> bulbs(N, std::vector<int>(2)); // Stores the states of the bulbs up to i
+//    std::vector<bool> bulbs(N);
     for (int i = 0; i < N; i++) {
         bool b; std::cin >> b;
         bool ans = valForL(i, K, X);
-        bulbs[i] = b ^ ans;
+        bool is_correct = !(b ^ ans);
+        if (i == 0) {
+            bulbs[i][is_correct] = 0;
+            bulbs[i][!is_correct] = 1; // switch
+        } else {
+            bulbs[i][is_correct] = bulbs[i - 1][is_correct];
+            bulbs[i][!is_correct] = bulbs[i - 1][!is_correct] + 1; // switch
+        }
+        if (i % K == K - 1) {
+            // edge, we may swap it all
+            bulbs[i][0] = std::min(bulbs[i][0], bulbs[i][1] + 1);
+            bulbs[i][1] = std::min(bulbs[i][1], bulbs[i][0] + 1);
+        }
 //        std::cerr << b << " ^ " << ans << " = " << bulbs[i];
     }
-    int flipCount = 0;
-    // Take the singles out
-    if (K == 1) {
-        for (int y = 1; y < N - 1; y++) {
-            // slide window
-            int x = y - 1; int z = y + 1;
-            // flip singles
-            if (bulbs[x] == bulbs[z] && bulbs[x] != bulbs[y]) {
-                // flip bulby
-                bulbs[y] = !bulbs[y];
-                flipCount++;
-//            std::cerr << "Manual flip " << y << std::endl;
-            }
-        }
-        if (bulbs[0] != bulbs[1]) {
-            bulbs[0] = !bulbs[0];
-            flipCount++;
-        }
-        if (bulbs[N - 2] != bulbs[N - 1]) {
-            bulbs[N - 1] = !bulbs[N - 1];
-            flipCount++;
-        }
-    } else {
-        // groups of K, make all 0's or all 1's
-        for (int a = 0; a < N / K; a++) {
-            int oneCount = 0;
-            for (int b = a * K; b < a * K + K; b++) {
-                // Groups of K
-//                std::cerr << "G " << b << std::endl;
-                if (bulbs[b]) {
-                    oneCount++;
-                }
-            }
-//            std::cerr << "OC " << oneCount << std::endl;
-            if (oneCount > 0) {
-//                int cutOff = K % 2 == 0 ? K / 2 + 1 : K / 2 + 2;
-                if (oneCount < K / 2 + 1) {
-                    // make them zero
-                    for (int b = a * K; b < a * K + K; b++) {
-                        bulbs[b] = false;
-                    }
-                    flipCount += oneCount;
-                } else {
-                    // make them one
-                    for (int b = a * K; b < a * K + K; b++) {
-                        bulbs[b] = true;
-                    }
-                    flipCount += K - oneCount;
-                }
-            }
 
-        }
-    }
-
-    bool hasSeenZero = false;
-    for (int i = 0; i <)
-    for (int i = 0; i < N; i++) {
-        bool b = bulbs[i];
-//        std::cerr << "Va " << valForL(i, K, X) << std::endl;
-//        std::cerr << b << " ";
-        if (b) {
-            oneCount++;
-        } else if (!b && oneCount > 0) {
-            // 0 came after at least a 1
-//            std::cerr << "One count " << oneCount << std::endl;
-            if (!hasSeenZero) {
-                flipCount += 1;
-            } else if (oneCount == 1) {
-                std::cerr << "Can not happend " << std::endl;
-                abort();
-            } else {
-                flipCount += 2;
-            }
-            oneCount = 0;
-        }
-
-        if (!b) {
-            hasSeenZero = true;
-        }
-    }
-    if (oneCount > 0) {
-        if (oneCount == 1) {
-            // add One
-            std::cerr << "Can not happend " << std::endl;
-            abort();
-            flipCount += 2;
-        } else {
-            flipCount += 2; // using flip-flip
-        }
-    }
-    std::cout << flipCount << std::endl;
+    std::cout << bulbs[N - 1][1] << std::endl;
 }
 
 int main() {
