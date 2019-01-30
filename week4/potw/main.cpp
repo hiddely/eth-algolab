0f44611f2134e8be0c8bc069e2def0bc724bd492
Hidde L <mail.hidde@gmail.com>
2019-01-30 08:49:50 +0100

    Fix hiking_maps

--- week4/potw/main.cpp
+++ week4/potw/main.cpp
@@ -1,150 +1,141 @@
 //
-// Created by Hidde Lycklama on 10/15/18.
+// Created by Hidde Lycklama on 1/17/19.
 //
 
-#include <iostream>
-#include <vector>
 #include <CGAL/Exact_predicates_inexact_constructions_kernel.h>
+#include <iostream>
+#include <stdexcept>
+#include <vector>
+#include <climits>
 #include <set>
+#include <tuple>
 
 typedef CGAL::Exact_predicates_inexact_constructions_kernel K;
-typedef K::Point_2 Point;
-typedef K::Segment_2 Seg;
-typedef K::Triangle_2 Tri;
-typedef K::Line_2 Line;
+typedef K::Point_2 P;
+typedef K::Segment_2 S;
+//typedef K::Triangle_2 T;
+typedef K::Line_2 L;
+typedef std::tuple<P, P, P, P, P, P> Tri;
 
-Point getPoint() {
-    int x, y;
-    std::cin >> x >> y;
-    return Point(x, y);
+P getPoint() {
+    int x, y; std::cin >> x >> y;
+    return P(x, y);
 }
 
-Line getEdge() {
-    return Line(getPoint(), getPoint());
+Tri createTriangle() {
+    P p1 = getPoint();
+    P p2 = getPoint();
+    P p3 = getPoint();
+    P p4 = getPoint();
+    P p5 = getPoint();
+    P p6 = getPoint();
+
+    if (!CGAL::left_turn(p1, p2, p3)) { // Important triangle learnings!!!
+        std::swap(p1, p2);
+    }
+    if (!CGAL::left_turn(p3, p4, p5)) {
+        std::swap(p3, p4);
+    }
+    if (!CGAL::left_turn(p5, p6, p1)) {
+        std::swap(p5, p6);
+    }
+    return Tri(p1, p2, p3, p4, p5, p6);
+//    std::cerr << "Invlaid triangle" <<std::endl;
+//    abort();
 }
 
-std::vector<Line> getEdges() {
-    std::vector<Line> edges(3);
-    for (int i = 0; i < 3; i++) {
-        edges[i] = getEdge();
-    }
-    return edges;
-}
+bool intersects(Tri &tri, S &s) {
+    P p1 = std::get<0>(tri);
+    P p2 = std::get<1>(tri);
+    P p3 = std::get<2>(tri);
+    P p4 = std::get<3>(tri);
+    P p5 = std::get<4>(tri);
+    P p6 = std::get<5>(tri);
 
-Point getIntersection(Line seg1, Line seg2) {
-    if (!CGAL::do_intersect(seg1, seg2)) {
-        std::cout << "Error parsing triangle segments" << std::endl;
-        throw std::invalid_argument( "received segment" );
-    }
-    // check intersection
-    auto o = CGAL::intersection(seg1, seg2);
-    if (const Point* op = boost::get<Point>(&*o)) {
-        return *op;
-    }
-    else if (const Line* os = boost::get<Line>(&*o)) {
-        std::cout << "Error parsing triangle segments, found segment" << std::endl;
-        throw std::invalid_argument( "received segment" );
-    }
-}
+//    std::cerr << !CGAL::right_turn(p1, p2, s.source()) << " " << !CGAL::right_turn(p3, p4, s.source()) << " " << !CGAL::right_turn(p5, p6, s.source())  << " " <<
+//    !CGAL::right_turn(p1, p2, s.target())  << " " << !CGAL::right_turn(p3, p4, s.target())  << " " << !CGAL::right_turn(p5, p6, s.target()) << std::endl;
 
-std::vector<Point> loadPoints() {
-    std::vector<Point> points(3);
-    std::vector<Line> edges = getEdges();
-    points[0] = getIntersection(edges[0], edges[1]);
-    points[1] = getIntersection(edges[1], edges[2]);
-    points[2] = getIntersection(edges[2], edges[0]);
-    return points;
+    return !CGAL::right_turn(p1, p2, s.source()) && !CGAL::right_turn(p3, p4, s.source()) && !CGAL::right_turn(p5, p6, s.source()) &&
+           !CGAL::right_turn(p1, p2, s.target()) && !CGAL::right_turn(p3, p4, s.target()) && !CGAL::right_turn(p5, p6, s.target());
 }
 
 void testcase() {
     int m, n;
     std::cin >> m >> n;
 
-    std::vector<Seg> path(m - 1);
-    int startX, startY;
-    std::cin >> startX >> startY;
-    Point start(startX, startY);
-    for (int i = 0; i < m - 1; i++) {
-        int x, y;
-        std::cin >> x >> y;
-        Point p(x, y);
-        path[i] = Seg(start, p);
-        start = p;
+    std::vector<S> path(m - 1);
+    P prev; std::cin >> prev;
+    for (int i = 1; i < m; i++) {
+        P cur; std::cin >> cur;
+        path[i - 1] = S(prev, cur);
+        prev = cur;
     }
 
-    std::vector<Tri> tri(n);
+    std::vector<Tri> parts(n);
     for (int i = 0; i < n; i++) {
-        std::vector<Point> p = loadPoints();
-        tri[i] = Tri(p[0], p[1], p[2]);
-//        std::cout << "P " << p[0] << " " << p[1] << " " << p[2] << std::endl;
+        parts[i] = createTriangle();
+//        std::cerr << parts[i] << std::endl;
     }
 
-    std::vector<std::set<int>> pathsInTriangles(n, std::set<int>());
-    int lowerBound = 0;
-    for (int a = 0; a < n; a++) { // triangles
-        for (int b = 0; b < m - 1; b++) { // legs
-            auto s = tri[a].bounded_side(path[b].source());
-            auto t = tri[a].bounded_side(path[b].target());
-            if ((s == CGAL::ON_BOUNDED_SIDE || s == CGAL::ON_BOUNDARY) &&
-                    (t == CGAL::ON_BOUNDED_SIDE || t == CGAL::ON_BOUNDARY)) {
-                pathsInTriangles[a].insert(b);
-//                std::cout << path[b] << " >> " << tri[a] << " " << "Found overlap at " << a << std::endl;
+//    std::cerr << "precover " << std::endl;
+
+    // pre compute who covers what
+    std::vector<std::vector<int>> covers(n); // Map part -> path
+    for (int i = 0; i < n; i++) {
+        for (int a = 0; a < m-1; a++) {
+            if (intersects(parts[i], path[a])) {
+//                std::cerr << i << ": intersects with " << path[a] << std::endl;
+                covers[i].push_back(a);
             }
         }
     }
 
-    std::vector<int> pathCount(m - 1, 0);
+//    std::cerr << "precheck " << std::endl;
 
-    int a = 0; int b = 0;
-    int k = n - 1;
-    while (a < n) {
-//        std::cout << "Visiting (" << a << "," << b << ")" << std::endl;
-        if (b == n || *std::min_element(pathCount.begin(), pathCount.end()) != 0) {
-            // incr a
-//            std::cout << "lb" << std::endl;
-            auto it = pathsInTriangles[a].cbegin();
-            while (it != pathsInTriangles[a].cend()) {
-                pathCount[*it]--;
+//    std::vector<bool> covered
+    int upper = 0; int lower = 0;
+    int minCost = INT_MAX;
 
-                if (pathCount[*it] < 0) {
-                    std::cout << "This can never happen" << std::endl;
+    std::vector<bool> covered(m-1, false);
+    std::vector<bool> coveredCheck(m-1, true);
+    std::vector<std::set<int>> coveredBy(m - 1);
+
+    while (upper <= n) {
+
+//        std::cerr << lower << "-" << upper << std::endl;
+        if (covered == coveredCheck) {
+            // increase lower
+            // record
+            minCost = std::min(minCost, upper - lower);
+            lower++;
+
+            for (auto iter = covers[lower - 1].begin(); iter != covers[lower - 1].end(); iter++) {
+                coveredBy[*iter].erase(lower - 1);
+                if (coveredBy[*iter].empty()) {
+                    covered[*iter] = false;
                 }
-
-                it++;
             }
-            a++;
+        } else if (upper == n) {
+            break;
         } else {
-            // add items to array
-//            std::cout << "ub" << std::endl;
-            auto it = pathsInTriangles[b].cbegin();
-            while (it != pathsInTriangles[b].cend()) {
-                pathCount[*it]++;
-                it++;
+            // increase upper
+            upper++;
+            for (auto iter = covers[upper - 1].begin(); iter != covers[upper - 1].end(); iter++) {
+                coveredBy[*iter].insert(upper - 1);
+                covered[*iter] = true;
             }
-            b++;
         }
-        if (*std::min_element(pathCount.begin(), pathCount.end()) != 0) {
-            // wew
-//            std::cout << "Found candidate!" << a << " " << b << std::endl;
-            k = std::min(k, b - a);
-        }
+
     }
 
-    // Tri 1: 0, 1
-    // Tri 2: -
-    // Tri 3: 1, 2
-    // Tri 4: 0
-
-    std::cout << k << std::endl;
-
-    // 0, 10
-    // 5, 8
-
+    std::cout << minCost << std::endl;
 }
 
 int main() {
+    std::ios_base::sync_with_stdio(false);
     int t; std::cin >> t;
-    for (; t > 0; t--) {
+    while(t-- > 0) {
         testcase();
     }
+    return 0;
 }
