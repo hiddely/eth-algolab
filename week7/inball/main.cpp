161f2726542d73df3a187905fb483e46f8608617
Hidde L <mail.hidde@gmail.com>
2019-01-30 15:01:32 +0100

    Add new version of inball

--- week7/inball/main.cpp
+++ week7/inball/main.cpp
@@ -33,14 +33,34 @@ int main() {
             break;
         }
         int d; std::cin >> d;
-        int n; std::cin >> n;
 
         const int R = d;
 
-        Program
+        Program lp(CGAL::SMALLER, false, 0, false, 0); // other vars may be nonnegative!
+        lp.set_c(R, -1); // minimize
+        lp.set_l(R, true, 0); //
 
         for (int i = 0; i < n; i++) {
+            double norm = 0;
+            for (int j = 0; j < d; j++) {
+                int a; std::cin >> a;
+                lp.set_a(j, i, a);
+                norm += a * a;
+            }
+            int b; std::cin >> b;
+            lp.set_b(i, b);
 
+            lp.set_a(R, i, std::floor(std::sqrt(norm))); // set R
+        }
+
+        Solution sol = CGAL::solve_linear_program(lp, ET());
+
+        if (sol.status() == CGAL::QP_INFEASIBLE) {
+            std::cout << "none" << std::endl;
+        } else if (sol.status() == CGAL::QP_UNBOUNDED) {
+            std::cout << "inf" << std::endl;
+        } else {
+            std::cout << floor_to_double(-sol.objective_value()) << std::endl;
         }
 
     }
